84d0a2fd23f2dd82a12278740b37decee86ad08d
Naitchi <assasindu39@gmail.com>
2026-04-28 15:42:59 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from llm_sdk import Small_LLM_Model
-from parsing import is_valid_filename
+from parsing import is_valid_filename, check_json_functions
 import json
 
 def generate_next_token(llm: Small_LLM_Model, tokens: list[int]) -> list[int]:
     possible_token = llm.get_logits_from_input_ids(tokens)
     max_value = max(possible_token)
+    print(max_value)
     next_token = [key for key, elem in enumerate(possible_token) if max_value == elem]
     tokens.append(next_token[0])
     return tokens
@@ -19,26 +20,52 @@ def get_functions_json(filename: str = "functions_definition.json") -> str:
             "r"
         ) as original:
             available_functions = json.load(original)
-            print(available_functions)
-
+            check_json_functions(available_functions)
+            return available_functions
     except (FileNotFoundError, FileExistsError, ValueError, Exception) as e:
         print(e)
 
+def get_prompts_json(filename: str = "function_calling_tests.json") -> list[dict]:
+    try:
+        is_valid_filename(filename)
+        with open(
+            f"../data/input/{filename}",
+            "r"
+        ) as original:
+            return json.load(original)
+    except (FileNotFoundError, FileExistsError, ValueError, Exception) as e:
+        print(e)
 
-def create_prompt() -> str:
-    pass
+def create_prompt(prompt: str, available_functions: str) -> str:
+    if len(prompt) < 1:
+        raise ValueError("Error the prompt cannot be empty")
+    return "".join([
+        "With this prompt: \"", 
+        prompt,
+        "\" Use this list of function: ", 
+        available_functions, 
+        " and return only JSON with prompt (prompt), the name of the function (name) you will use, and the parameters (parameters). The Answer: { \"prompt\": \"",
+        prompt,
+        "\", \"name\": "
+    ])
 
 
 def main():
-    # jarvis = Small_LLM_Model()
-    # prompt:str = create_prompt()
-    # "Do the calcul: '2+2'? Give me just the result. No explanation. Just the number. The Result is :"
-    # tokens = jarvis.encode(prompt)
-    # converted_list = list(tokens[0])
-    get_functions_json()
-    # while 1:
-    #     generate_next_token(jarvis, converted_list)
-    #     print(jarvis.decode(converted_list))
-    
+    # TODO faire pour qu'on recupere les args et les passe dans get_functions_json et get_prompt_json (changer pour verifier les donnees avant et pas dans les fonctions comme maintenant(sale))
+    # "Do the calcul: '2x8'? Give me just the result. No explanation. Just the number. Put a point after your response. The Result is :"
+    available_functions = get_functions_json()
+    prompts = get_prompts_json()
+    jarvis = Small_LLM_Model()
+    for user_prompt in prompts:
+        try :
+            prompt:str = create_prompt(user_prompt["prompt"], str(available_functions))
+            tokens = jarvis.encode(prompt)
+            print(tokens)
+            converted_list = list(tokens[0])
+            while 1:
+                generate_next_token(jarvis, converted_list)
+                print(jarvis.decode(converted_list))
+        except (FileNotFoundError, FileExistsError, ValueError, Exception) as e:
+            print(e)
 
 main()
--- src/parsing.py
+++ src/parsing.py
@@ -1,20 +1,35 @@
 import re
 from pydantic import BaseModel, Field
-from typing import Dict, Literal
+from enum import Enum
 
-ParamType = Literal["number", "string", "boolean"]
+class AllowedType(str, Enum):
+    NUMBER = "number"
+    STRING = "string"
+    BOOLEAN = "boolean"
+    INTEGER = "integer"
+    ARRAY = "array"
+    OBJECT = "object"
+    NULL = "null"
 
-class Parameter(BaseModel):
-    type: ParamType = Field(..., description="Type of the parameter")
+class TypedField(BaseModel):
+    type: AllowedType
 
-class FunctionSpec(BaseModel):
-    name: str = Field(...,min_length=1, pattern=r"^[a-zA-Z0-9_]$")
-    description: str = Field(..., min_length=1)
-    parameters: Dict[str, Parameter] = Field(...)
-    returns: Parameter = Field(...)
+class FunctionDefinition(BaseModel):
+    name: str = Field(strict=True, min_length=1)
+    description: str = Field(strict=True, min_length=1)
+    parameters: dict[str, TypedField] = Field(strict=True, min_length=1)
+    returns: TypedField
+
+class PromptDefinition(BaseModel):
+    prompt:str = Field()
+
+def check_json_functions(list_function_dict:list[dict])->None:
+    for fx in list_function_dict:
+        FunctionDefinition(**fx)
+
 
 def is_valid_filename(filename: str) -> None:
     pattern: str = r'^[a-zA-Z0-9_-]+\.json$'
-    if not re.match(pattern,filename):
-        raise ValueError("Json name invalide. Please rename it in something like: [a-zA-Z0-9_-]+\.json")
+    if not re.fullmatch(pattern, filename):
+        raise ValueError(r"Json name invalide. Please rename it in something like: [a-zA-Z0-9_-]+\.json")
 
